06204de6bd52503cd5c622bae255e59f616fd183
Geoff Root <groot314@gmail.com>
2018-09-27 22:20:14 -0700

    Tested mysql connection

---
+++ PaidSickLeaveTracker/PaidSickLeaveTracker/ConnectDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace PaidSickLeaveTracker
+{
+	class ConnectDB
+	{
+		public ConnectDB()
+		{
+
+		}
+
+		MySqlConnection con = new MySqlConnection("Data Source=;Initial Catalog=SickHours;Persist Security Info=True; User ID=; Password=");
+
+		public MySqlConnection Connection
+		{
+			get { return con; }
+		}
+
+	}
+}
--- PaidSickLeaveTracker/PaidSickLeaveTracker/Program.cs
+++ PaidSickLeaveTracker/PaidSickLeaveTracker/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Diagnostics;
 
 namespace PaidSickLeaveTracker
 {
@@ -14,6 +17,23 @@ namespace PaidSickLeaveTracker
 		[STAThread]
 		static void Main()
 		{
+			ConnectDB db = new ConnectDB();
+
+			MySqlCommand test = new MySqlCommand("Insert Into test (name) Values ('Geoff')", db.Connection);
+
+			try
+			{
+				if(db.Connection.State == ConnectionState.Closed)
+				{
+					db.Connection.Open();
+				}
+				test.ExecuteNonQuery();
+			}
+			catch (Exception ex)
+			{
+				Debug.Write(ex.Message);
+			}
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
