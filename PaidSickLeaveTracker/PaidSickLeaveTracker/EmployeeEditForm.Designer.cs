71512ccbdac1937442d822fd7ff54646384d668b
Geoff Root <groot314@gmail.com>
2018-09-28 15:34:41 -0700

    Basic functionality on Employees working with Mysql

--- PaidSickLeaveTracker/PaidSickLeaveTracker/EmployeeEditForm.Designer.cs
+++ PaidSickLeaveTracker/PaidSickLeaveTracker/EmployeeEditForm.Designer.cs
@@ -28,60 +28,67 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.removeButton = new System.Windows.Forms.Button();
-			this.EditButton = new System.Windows.Forms.Button();
-			this.employeeNameTxt = new System.Windows.Forms.TextBox();
-			this.EmployeeDDL = new System.Windows.Forms.ComboBox();
-			this.SuspendLayout();
-			// 
-			// removeButton
-			// 
-			this.removeButton.Location = new System.Drawing.Point(248, 134);
-			this.removeButton.Name = "removeButton";
-			this.removeButton.Size = new System.Drawing.Size(75, 23);
-			this.removeButton.TabIndex = 9;
-			this.removeButton.Text = "Remove";
-			this.removeButton.UseVisualStyleBackColor = true;
-			// 
-			// EditButton
-			// 
-			this.EditButton.Location = new System.Drawing.Point(166, 134);
-			this.EditButton.Name = "EditButton";
-			this.EditButton.Size = new System.Drawing.Size(75, 23);
-			this.EditButton.TabIndex = 8;
-			this.EditButton.Text = "Edit";
-			this.EditButton.UseVisualStyleBackColor = true;
-			// 
-			// employeeNameTxt
-			// 
-			this.employeeNameTxt.Location = new System.Drawing.Point(59, 134);
-			this.employeeNameTxt.Name = "employeeNameTxt";
-			this.employeeNameTxt.Size = new System.Drawing.Size(100, 22);
-			this.employeeNameTxt.TabIndex = 7;
-			// 
-			// EmployeeDDL
-			// 
-			this.EmployeeDDL.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.EmployeeDDL.FormattingEnabled = true;
-			this.EmployeeDDL.Location = new System.Drawing.Point(59, 83);
-			this.EmployeeDDL.Name = "EmployeeDDL";
-			this.EmployeeDDL.Size = new System.Drawing.Size(121, 24);
-			this.EmployeeDDL.TabIndex = 10;
-			// 
-			// EmployeeEditForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(446, 303);
-			this.Controls.Add(this.EmployeeDDL);
-			this.Controls.Add(this.removeButton);
-			this.Controls.Add(this.EditButton);
-			this.Controls.Add(this.employeeNameTxt);
-			this.Name = "EmployeeEditForm";
-			this.Text = "EditEmployeeForm";
-			this.Load += new System.EventHandler(this.EmployeeEditForm_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.employeeNameTxt = new System.Windows.Forms.TextBox();
+            this.employeeDDL = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(186, 109);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(56, 19);
+            this.removeButton.TabIndex = 9;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(124, 109);
+            this.EditButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(56, 19);
+            this.EditButton.TabIndex = 8;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // employeeNameTxt
+            // 
+            this.employeeNameTxt.Location = new System.Drawing.Point(44, 109);
+            this.employeeNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.employeeNameTxt.Name = "employeeNameTxt";
+            this.employeeNameTxt.Size = new System.Drawing.Size(76, 20);
+            this.employeeNameTxt.TabIndex = 7;
+            // 
+            // employeeDDL
+            // 
+            this.employeeDDL.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.employeeDDL.FormattingEnabled = true;
+            this.employeeDDL.Location = new System.Drawing.Point(44, 67);
+            this.employeeDDL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.employeeDDL.Name = "employeeDDL";
+            this.employeeDDL.Size = new System.Drawing.Size(92, 21);
+            this.employeeDDL.TabIndex = 10;
+            // 
+            // EmployeeEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 246);
+            this.Controls.Add(this.employeeDDL);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.employeeNameTxt);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "EmployeeEditForm";
+            this.Text = "EditEmployeeForm";
+            this.Load += new System.EventHandler(this.EmployeeEditForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -90,6 +97,6 @@
 		private System.Windows.Forms.Button removeButton;
 		private System.Windows.Forms.Button EditButton;
 		private System.Windows.Forms.TextBox employeeNameTxt;
-		private System.Windows.Forms.ComboBox EmployeeDDL;
+		private System.Windows.Forms.ComboBox employeeDDL;
 	}
 }
--- PaidSickLeaveTracker/PaidSickLeaveTracker/EmployeeEditForm.cs
+++ PaidSickLeaveTracker/PaidSickLeaveTracker/EmployeeEditForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 
 namespace PaidSickLeaveTracker
 {
@@ -19,7 +22,44 @@ namespace PaidSickLeaveTracker
 
 		private void EmployeeEditForm_Load(object sender, EventArgs e)
 		{
+            ConnectDB dbcon = new ConnectDB();
+            MySqlDataAdapter selectEmployees = new MySqlDataAdapter("Select * From Employees", dbcon.Connection);
 
+
+            DataTable dt = new DataTable();
+
+            selectEmployees.Fill(dt);
+
+            employeeDDL.DataSource = dt;
+            employeeDDL.DisplayMember = "Name";
+            employeeDDL.ValueMember = "EmployeeID";
+
+            
+            employeeNameTxt.Text = employeeDDL.Text;
+              
 		}
-	}
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            ConnectDB dbcon = new ConnectDB();
+
+            MySqlCommand updateEmployee = new MySqlCommand("Update Employees SET Name=@name WHERE EmployeeID=@id", dbcon.Connection);
+
+            updateEmployee.Parameters.AddWithValue("@name", employeeNameTxt.Text);
+            updateEmployee.Parameters.AddWithValue("@id", employeeDDL.SelectedValue);
+
+            dbcon.runCommand(updateEmployee);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            ConnectDB dbcon = new ConnectDB();
+
+            MySqlCommand updateEmployee = new MySqlCommand("Delete From Employees WHERE EmployeeID=@id", dbcon.Connection);
+
+            updateEmployee.Parameters.AddWithValue("@id", employeeDDL.SelectedValue);
+
+            dbcon.runCommand(updateEmployee);
+        }
+    }
 }
